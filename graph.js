d1bd1f901706e6f84a908604ba5d4cf59bbd58e8
CGF <cgf1993@foxmail.com>
2017-06-14 10:15:15 +0800

    gao

---
+++ graph.js
@@ -0,0 +1,57 @@
+var json = {
+  "nodes": [
+    {
+      "color": "#4f19c7",
+      "label": "1",
+      "attributes": {},
+      "y": 404.26147,
+      "x": 739.36383,
+      "id": "1",
+      "size": 94.7252817
+    },
+    {
+      "color": "#c74f19",
+      "label": "2",
+      "attributes": {},
+      "y": 321.36972,
+      "x": 618.49976,
+      "id": "2",
+      "size": 5.52601284
+    },
+    {
+      "color": "#c84d19",
+      "label": "3",
+      "attributes": {},
+      "y": 21.36972,
+      "x": 18.49976,
+      "id": "3",
+      "size": 7.52601284
+    }
+  ],
+  "edges": [
+    {
+      "sourceID": "1",
+      "attributes": {},
+      "targetID": "2",
+      "size": 1
+    },
+    {
+      "sourceID": "2",
+      "attributes": {},
+      "targetID": "1",
+      "size": 10
+    },
+    {
+      "sourceID": "2",
+      "attributes": {},
+      "targetID": "3",
+      "size": 8
+    },
+    {
+      "sourceID": "3",
+      "attributes": {},
+      "targetID": "1",
+      "size": 8
+    },
+  ]
+}
